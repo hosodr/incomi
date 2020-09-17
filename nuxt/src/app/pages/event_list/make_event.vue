91b07396c872eb405692802f1a9045c12dad5ab7
kheminw <khem_inw@protonmail.ch>
2020-09-17 15:21:25 +0900

    edit template

--- nuxt/src/app/pages/event_list/make_event.vue
+++ nuxt/src/app/pages/event_list/make_event.vue
@@ -1,31 +1,66 @@
 <template>
   <div class="container">
+    <div class="row justify-content-center">
+      <h1>Create a new event</h1>
+    </div>
     <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-        />
-        <small id="emailHelp" class="form-text text-muted"
-          >We'll never share your email with anyone else.</small
-        >
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="eventName">
+          Event Name
+        </label>
+        <div class="col-sm-10">
+          <input
+            id="eventName"
+            v-model="eventName"
+            type="text"
+            class="form-control"
+            placeholder="a cool name for your event"
+          />
+        </div>
       </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-          placeholder="Password"
-        />
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="eventDescription">
+          Description
+        </label>
+        <div class="col-sm-10">
+          <input
+            id="eventDescription"
+            v-model="eventDescription"
+            type="text"
+            class="form-control"
+            placeholder="a short description for your event"
+          />
+        </div>
       </div>
-      <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="eventDatePicker">
+          Event Date
+        </label>
+        <div class="col-sm-10">
+          <b-form-datepicker
+            id="eventDatePicker"
+            v-model="eventDate"
+          ></b-form-datepicker>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="startDatePicker">
+          Period
+        </label>
+        <div class="col-md-5">
+          <b-form-datepicker
+            id="startDatePicker"
+            v-model="startDate"
+            placeholder="Start Date"
+          ></b-form-datepicker>
+        </div>
+        <div class="col-md-5">
+          <b-form-datepicker
+            id="endDatePicker"
+            v-model="endDate"
+            placeholder="End Date"
+          ></b-form-datepicker>
+        </div>
       </div>
       <button type="button" class="btn btn-primary" @click="submit">
         Submit
@@ -36,6 +71,15 @@
 
 <script>
 export default {
+  data: () => {
+    return {
+      eventName: '',
+      eventDescription: '',
+      eventDate: '',
+      startDate: '',
+      endDate: '',
+    }
+  },
   methods: {
     submit() {
       this.$router.replace('1')
