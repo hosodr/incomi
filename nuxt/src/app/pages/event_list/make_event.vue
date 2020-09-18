54d59c72e26db354e990a52597dda19017a738ff
kheminw <khem_inw@protonmail.ch>
2020-09-18 15:26:12 +0900

    improve datetime field

--- nuxt/src/app/pages/event_list/make_event.vue
+++ nuxt/src/app/pages/event_list/make_event.vue
@@ -36,11 +36,18 @@
         <label class="col-sm-2 col-form-label" for="eventDatePicker">
           Event Date
         </label>
-        <div class="col-sm-10">
+        <div class="col-sm-6">
           <b-form-datepicker
             id="eventDatePicker"
             v-model="eventDate"
+            :min="today"
           ></b-form-datepicker>
+        </div>
+        <div class="col-md-4">
+          <b-form-timepicker
+            v-model="eventTime"
+            locale="en"
+          ></b-form-timepicker>
         </div>
       </div>
       <div class="form-group row">
@@ -51,6 +58,7 @@
           <b-form-datepicker
             id="startDatePicker"
             v-model="startDate"
+            :min="today"
             placeholder="Start Date"
           ></b-form-datepicker>
         </div>
@@ -58,13 +66,23 @@
           <b-form-datepicker
             id="endDatePicker"
             v-model="endDate"
+            :min="startDate"
             placeholder="End Date"
           ></b-form-datepicker>
         </div>
       </div>
-      <button type="button" class="btn btn-primary" @click="submit">
-        Submit
-      </button>
+      <div class="form-group row">
+        <div class="col-md-2">
+          <button type="button" class="btn btn-primary" @click="submit">
+            Submit
+          </button>
+        </div>
+        <div class="col-md-2">
+          <button type="button" class="btn btn-primary" @click="cancel">
+            Cancel
+          </button>
+        </div>
+      </div>
     </form>
   </div>
 </template>
@@ -76,13 +94,30 @@ export default {
       eventName: '',
       eventDescription: '',
       eventDate: '',
+      eventTime: '',
       startDate: '',
       endDate: '',
     }
   },
+  computed: {
+    today() {
+      return new Date()
+    },
+    eventDateTime() {
+      const date = new Date(this.eventDate)
+      const time = this.eventTime.split(':')
+      date.setHours(time[0])
+      date.setMinutes(time[1])
+      date.setSeconds(time[2])
+      return date
+    },
+  },
   methods: {
     submit() {
       this.$router.replace('1')
+    },
+    cancel() {
+      this.$router.back()
     },
   },
 }
