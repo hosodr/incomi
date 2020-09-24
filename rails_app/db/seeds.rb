735ff25dd31c5be79b3cd53c209aa7ef9d5c0002
大槻 優介 <otsuki_yusuke@plise.co.jp>
2020-09-25 02:03:13 +0900

    add seeds

--- rails_app/db/seeds.rb
+++ rails_app/db/seeds.rb
@@ -15,7 +15,9 @@ channel1 = Channel.create!(name:"Machine Learning", abstract:'for beginner')
 channel2 = Channel.create!(name:"Statistics", abstract:'for beginner')
 channel3 = Channel.create!(name:"Tour of GO", abstract:'I fully understood golang')
 channel4 = Channel.create!(name:"What is gRPC?", abstract:'tell me what gRPC is.')
-
+channel5 = Channel.create!(name:"Let's study JavaScript", abstract:'I cannot understand Javascript')
+channel6 = Channel.create!(name:"Java", abstract:'basic')
+channel7 = Channel.create!(name:"framework", abstract:'advanced')
 
 event1 = Event.create!(channel_id:1,host_user_id:4 ,name:'GAN')
 event2 = Event.create!(channel_id:1,host_user_id:2 ,name:'How to use R')
@@ -26,11 +28,26 @@ comment2 = Comment.create!(user_id:4, channel_id:1, message: 'See this twitter: 
 channel5 = Channel.create!(parent_channel_id:1, parent_comment_id:2)
 comment3 = Comment.create!(user_id:1, channel_id:5, message: "what's this?")
 comment4 = Comment.create!(user_id:4, channel_id:5, message: "I've created event")
-comment5 = Comment.create!(user_id:5, channel_id:1, message: 'Twitter!?')
+comment5 = Comment.create!(user_id:5, channel_id:8, message: 'Twitter!?')
 comment6 = Comment.create!(user_id:2, channel_id:3, message: 'what is this topic?')
 comment7 = Comment.create!(user_id:3, channel_id:3, message: 'this is your channel, right?')
+comment8 = Comment.create!(user_id:1, channel_id:6, message: "I didn't understand today's class")
+comment9 = Comment.create!(user_id:2, channel_id:6, message: "I lost my text book!")
+comment10 = Comment.create!(user_id:3, channel_id:6, message: "I overslept and couldn't attend class.")
+
+channel8 = Channel.create!(parent_channel_id:6, parent_comment_id:8)
+comment11 = Comment.create!(user_id:3, channel_id:9, message: "Why don't you study at the study meeting?")
+comment12 = Comment.create!(user_id:1, channel_id:9, message: "Yes!")
+comment13 = Comment.create!(user_id:1, channel_id:6, message: "It was decided to hold the study meeting. Everyone is welcome to join us.")
+comment14 = Comment.create!(user_id:5, channel_id:6, message: "If someone know the range of mid term exam, please tell me.")
+
+channel9 = Channel.create!(parent_channel_id:6, parent_comment_id:14)
+comment15 = Comment.create!(user_id:1, channel_id:10, message: "Maybe up to page 30")
+
+event4 = Event.create!(channel_id:6,host_user_id:3 ,name:"Let's study Java!", abstract:"Studing Java", from_date:'2020-10-01', to_date:'2020-10-02')
 
 
 participation1 = Participation.create!(user_id:1,event_id:1)
 participation2 = Participation.create!(user_id:4,event_id:1)
+# participation3 = Participation.create!(user_id:3,event_id:3)
 
